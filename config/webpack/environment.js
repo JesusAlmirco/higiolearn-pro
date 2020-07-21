1f01ba2bb3f391179e98f41ef09058556a08e1ef
Humsbel Jao <jao.deve@gmail.com>
2020-07-20 20:17:11 -0700

    Installed boostrap modules, fontfamily, css and implemented navbar

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
-
+//Integrate bootstrap compunents
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['Popper.js', 'default']
+}))
+//end integrate
 module.exports = environment
