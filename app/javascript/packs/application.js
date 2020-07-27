d80040c4b61e6f2ea36c53577329c4cd4a39bb19
Humsbel Jao <jao.deve@gmail.com>
2020-07-27 03:08:53 -0700

    Implemented notifications with toastr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("channels")
 //add bootstrap component
 require("bootstrap/dist/js/bootstrap")
 //end add
+//add toastr for notification messages
+global.toastr = require("toastr")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
