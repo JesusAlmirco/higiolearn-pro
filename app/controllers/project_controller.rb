f16f148160bce4b8b4c431b1046b7bf8cc36a151
Humsbel Jao <jao.deve@gmail.com>
2020-08-12 20:33:14 -0700

    Update project controller

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -6,5 +6,12 @@ class ProjectController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @tasks = @project.tasks.order(:tag)
+
+    #validate access to project with subscription
+    @joined = false
+
+    if !current_user.nil? && !current_user.projects.nil?
+      @joined = current_user.projects.include?(@project)
+    end
   end
 end
