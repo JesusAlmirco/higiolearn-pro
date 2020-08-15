a43a1e95d07884bff97b912b22d16e604c5ed049
Humsbel Jao <jao.deve@gmail.com>
2020-08-14 18:01:45 -0700

    Update task controller and views

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,11 +1,27 @@
 class TaskController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     project = Project.find(params[:project_id])
     @tasks = project.tasks.order(:tag)
     @task = @tasks.find(params[:id])
 
-    #next and prev buttons
-    @next_task = @task.next
-    @prev_task = @task.prev
+    #validate access to project with subscription
+    joined = false
+
+    if !current_user.nil? && !current_user.projects.nil?
+      joined = current_user.projects.include?(project)
+    end
+
+    #next and prev buttons  before buy the project
+    if joined
+      @task = @tasks.find(params[:id])
+      @next_task = @task.next
+      @prev_task = @task.prev
+    else
+      flash[:alert] = "No permission to this project!"
+      redirect_to project
+    end
+    
   end
 end
