10137b41e52ccbd07d48a3f9d4980118079963b6
Humsbel Jao <jao.deve@gmail.com>
2020-09-08 19:01:54 -0700

    Add aditional custom for typo [] nil- beacuse capistrano not compaile stripe credentials

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,9 @@
 Rails.configuration.stripe = {
+  #verify for not print [] nil
+  puts Rails.application.credentials.inspect,
+  puts Rails.application.credentials[Rails.env.to_sym].inspect,
+  puts Rails.application.credentials[Rails.env.to_sym][:stripe].inspect,
+  #stripe credentials
   :publishable_key => Rails.application.credentials[Rails.env.to_sym][:stripe][:STRIPE_PUBLIC_KEY],
   :secret_key      => Rails.application.credentials[Rails.env.to_sym][:stripe][:STRIPE_SECRET_KEY]
 }
