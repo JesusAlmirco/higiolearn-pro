f3206df546edc28750b64e51db63d3d84a84c6ad
Humsbel Jao <jao.deve@gmail.com>
2020-09-09 16:23:09 -0700

    Changed ip server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '161.35.54.104', user: 'deploy', roles: %w{app db web}
+server '206.189.204.254', user: 'jesus', roles: %w{app db web}
 
 # role-based syntax
 # ==================
