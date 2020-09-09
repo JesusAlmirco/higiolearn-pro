1cb04bf2b9f6c36ac134fcf0cceef199c9ab3356
Humsbel Jao <jao.deve@gmail.com>
2020-09-09 11:05:44 -0700

    Posible soluction to stripe credentials compile typo

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,4 +5,4 @@
 # }
 
 #Stripe.api_key = Rails.configuration.stripe[:secret_key]
-Stripe.api_key = Rails.application.credentials[Rails.env.to_sym][:stripe][:STRIPE_SECRET_KEY]
+Stripe.api_key = Rails.application.credentials.dig(Rails.env.to_sym, :stripe, :STRIPE_SECRET_KEY)
