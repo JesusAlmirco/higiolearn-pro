1811c8e82e7c586d949df0d61470ff0f2eccccfd
Humsbel Jao <jao.deve@gmail.com>
2020-09-09 10:31:32 -0700

    Correction typo of stripe credentials

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,8 @@
-Rails.configuration.stripe = {
-  #stripe credentials
-  :publishable_key => Rails.application.credentials[Rails.env.to_sym][:stripe][:STRIPE_PUBLIC_KEY],
-  :secret_key      => Rails.application.credentials[Rails.env.to_sym][:stripe][:STRIPE_SECRET_KEY]
-}
+#Rails.configuration.stripe = {
+#   stripe credentials
+#   :publishable_key => Rails.application.credentials[Rails.env.to_sym][:stripe][:STRIPE_PUBLIC_KEY],
+#   :secret_key      => Rails.application.credentials[Rails.env.to_sym][:stripe][:STRIPE_SECRET_KEY]
+# }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+#Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.application.credentials[Rails.env.to_sym][:stripe][:STRIPE_SECRET_KEY]
